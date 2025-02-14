5f746e925f7467d3e87b8a13f742a8652f72ef09
danastsiad <danastsiad@gmail.com>
2025-02-14 11:07:13 +0300

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="app-header">
+    <span class="logo">TODOS</span>
+  </header>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style></style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul class="todo-list">
+    <TodoItem
+      v-for="todo in todos"
+      :key="todo.id"
+      :todo="todo"
+      @toggleTodo="toggleTodo"
+      @removeTodo="removeTodo"
+    />
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, type PropType } from "vue";
+import TodoItem from "./TodoItem.vue";
+import { Todo } from "@/types/Todo";
+
+export default defineComponent({
+  components: {
+    TodoItem,
+  },
+  props: {
+    todos: {
+      type: Array as PropType<Todo[]>,
+    },
+  },
+  methods: {
+    toggleTodo(id: number) {
+      this.$emit("toggleTodo", id);
+    },
+    removeTodo(id: number) {
+      this.$emit("removeTodo", id);
+    },
+  },
+  emits: {
+    toggleTodo: (id: number) => Number.isInteger(id),
+    removeTodo: (id: number) => Number.isInteger(id),
+  },
+});
+</script>
+
+<style></style>
